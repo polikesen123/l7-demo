96beda42ccc22f0514bd2cd871564539a919eca1
xiaopf <xiaopf@syc.green>
2022-01-04 17:09:10 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,12 +14,16 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  width: 100vw;
+  height: 100vh;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+html,body{
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="mapContainer"></div>
 </template>
 
 <script>
+import { Scene, LineLayer, PointLayer } from "@antv/l7";
+import { Mapbox } from "@antv/l7-maps";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+    mounted() {
+        this.initMap();
+    },
+    methods: {
+        initMap() {
+            const scene = new Scene({
+                id: "mapContainer",
+                map: new Mapbox({
+                    pitch: 40,
+                    style: {
+                        version: 8,
+                        sprite: "https://lzxue.github.io/font-glyphs/sprite/sprite",
+                        glyphs: "https://gw.alipayobjects.com/os/antvdemo/assets/mapbox/glyphs/{fontstack}/{range}.pbf",
+                        sources: {},
+                        layers: [
+                            {
+                                id: "background",
+                                type: "background",
+                                paint: {
+                                    "background-color": "#2b2b3a",
+                                },
+                            },
+                        ],
+                    },
+                    center: [3.438, 40.16797],
+                    zoom: 0.51329,
+                }),
+            });
+            scene.on("loaded", () => {
+                Promise.all([
+                    fetch(
+                        "https://gw.alipayobjects.com/os/basement_prod/dbd008f1-9189-461c-88aa-569357ffc07d.json"
+                    ).then((d) => d.json()),
+                    fetch(
+                        "https://gw.alipayobjects.com/os/basement_prod/4472780b-fea1-4fc2-9e4b-3ca716933dc7.json"
+                    ).then((d) => d.text()),
+                    fetch(
+                        "https://gw.alipayobjects.com/os/basement_prod/a5ac7bce-181b-40d1-8a16-271356264ad8.json"
+                    ).then((d) => d.text()),
+                ]).then(function onLoad([world, dot, flyline]) {
+                    const dotData = eval(dot);
+                    const flydata = eval(flyline).map((item) => {
+                        const latlng1 = item.from.split(",").map((e) => {
+                            return e * 1;
+                        });
+                        const latlng2 = item.to.split(",").map((e) => {
+                            return e * 1;
+                        });
+                        return { coord: [latlng1, latlng2] };
+                    });
+                    // const worldFill = new PolygonLayer()
+                    //   .source(world)
+                    //   .color('#98E3FA')
+                    //   .shape('fill')
+                    //   .style({
+                    //     opacity: 1
+                    //   });
+
+                    const worldLine = new LineLayer()
+                        .source(world)
+                        .color("#41fc9d")
+                        .size(0.5)
+                        .style({
+                            opacity: 0.4,
+                        });
+                    const dotPoint = new PointLayer()
+                        .source(dotData, {
+                            parser: {
+                                type: "json",
+                                x: "lng",
+                                y: "lat",
+                            },
+                        })
+                        .shape("circle")
+                        .color("#ffed11")
+                        .animate(true)
+                        .size(40)
+                        .style({
+                            opacity: 1.0,
+                        });
+                    const flyLine = new LineLayer()
+                        .source(flydata, {
+                            parser: {
+                                type: "json",
+                                coordinates: "coord",
+                            },
+                        })
+                        .color("#ff6b34")
+                        .shape("arc3d")
+                        .size(2)
+                        .active(true)
+                        .animate({
+                            interval: 2,
+                            trailLength: 2,
+                            duration: 1,
+                        })
+                        .style({
+                            opacity: 1,
+                        });
+                    // scene.addLayer(worldFill);
+                    scene.addLayer(worldLine);
+                    scene.addLayer(dotPoint);
+                    scene.addLayer(flyLine);
+                });
+            });
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#mapContainer {
+    width: 100vw;
+    height: 100vh;
 }
 </style>
